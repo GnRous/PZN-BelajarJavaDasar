2126614ab9af60f1725d40589a080c769f441e94
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-10 08:59:52 +0800

    added class IfElseStatement

---
+++ src/com/pzn/javafundamental/IfElseStatement.java
@@ -0,0 +1,27 @@
+package com.pzn.javafundamental;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+
+        var nilai = 80;
+        var absen = 70;
+        
+        if (nilai >= 75 && absen >= 75) {
+            System.out.println("Selamat Anda Lulus");
+        } else {
+            System.out.println("Silahkan Coba lagi tahun depan");
+        }
+
+        if (nilai >= 80 && absen >= 80) {
+            System.out.println("Nilai Anda A");
+        } else if (nilai >= 70 && absen >= 70) {
+            System.out.println("Nilai Anda B");
+        } else if (nilai >= 60 && absen >= 60) {
+            System.out.println("Nilai Anda C");
+        } else if (nilai >= 50 && absen >= 50) {
+            System.out.println("Nilai Anda D");
+        } else {
+            System.out.println("Nilai Anda E");
+        }
+    }
+}
