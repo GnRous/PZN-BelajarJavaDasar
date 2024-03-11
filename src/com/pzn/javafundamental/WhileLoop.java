6fe8e8a529daca8601bd689bd95028e106981255
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-11 13:54:12 +0800

    added WhileLoop class

---
+++ src/com/pzn/javafundamental/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pzn.javafundamental;
+
+public class WhileLoop {
+  public static void main(String[] args) {
+    var counter = 1;
+
+    while (counter <= 10) {
+      System.out.println("Perulangan while ke-" + counter);
+
+      counter++;
+    }
+  }
+}
