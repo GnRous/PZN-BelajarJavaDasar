f3bfc2bf5f60c4449799fd5fd4f2d801fc1ac6c4
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-12 10:09:27 +0800

    added Komentar class

---
+++ src/com/pzn/javafundamental/Komentar.java
@@ -0,0 +1,32 @@
+package com.pzn.javafundamental;
+
+//ini adalah class untuk contoh pemanfaatan komentar
+public class Komentar {
+  public static void main(String[] args) {
+    System.out.println(sum(10, 10));
+  }
+
+  //ini komentar satu baris
+  /*
+  ini adalah komentar paragraf
+   */
+
+
+  /*
+  * sdbfkuidsf
+  * h suhdfi dg d
+  *  dsfgudsusdh ofgu
+  * */
+
+  /**
+   * Method untuk menjumlahkan value1 dan value2
+   * @param value1 angka pertama
+   * @param value2 angka kedua
+   * @return hasil penjumlahan value1 dan value2
+   */
+
+  static int sum(int value1, int value2) {
+    // jumlahkan value1 ditambah value2
+    return value1 + value2;
+  }
+}
