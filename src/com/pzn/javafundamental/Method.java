e9c48b01b94b76d8df8010fc64b182bc6dfde89d
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-12 08:30:43 +0800

    added Method class

---
+++ src/com/pzn/javafundamental/Method.java
@@ -0,0 +1,16 @@
+package com.pzn.javafundamental;
+
+public class Method {
+  public static void main(String[] args) {
+
+    sayHelloWorld();
+    sayHelloWorld();
+    sayHelloWorld();
+  }
+
+  static void sayHelloWorld() {
+    System.out.println("Hello World 1");
+    System.out.println("Hello World 2");
+    System.out.println("Hello World 3");
+  }
+}
